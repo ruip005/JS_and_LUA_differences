c602de36d50adaa6dc63725ec05065ae7af11c1c
Rui Rodrigues <pv28292@alunos.estgv.ipv.pt>
2023-12-09 17:36:52 +0000

    Add files via upload

---
+++ Operators/Ternary.js
@@ -0,0 +1,9 @@
+function checkNumber(number) {
+    // if (number % 2 === 0) {
+    //     return "Even";
+    // } else {
+    //     return "Odd";
+    return number % 2 === 0 ? "Even" : "Odd";
+}
+
+console.log(checkNumber(2)); // Output: Even
---
+++ Operators/Ternary.lua
@@ -0,0 +1,9 @@
+function max(a, b)
+    -- if a > b then
+    --     return a
+    -- else
+    --     return b
+    -- end
+    local max_value = (a > b) and a or b
+    return max_value
+end

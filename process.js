555eecbec92b0dc8507f29304956fbeb2a3240b9
Rui Rodrigues <pv28292@alunos.estgv.ipv.pt>
2023-12-09 17:27:59 +0000

    Add files via upload

---
+++ process.js
@@ -0,0 +1,9 @@
+console.log("Nome do ficheiro: ", __filename); // *
+console.log("Diretoria do ficheiro: ", __dirname); // *
+console.log("Argumentos recebidos: ", process.argv); // *
+console.log("Diretoria de trabalho: ", process.cwd());
+console.log("Sistema operativo: ", process.platform); // *
+console.log("Versão do Node: ", process.version);
+console.log("Uso da memória: ", process.memoryUsage());
+console.log("Process ID: ", process.pid);
+console.log("Pasta corrente: ", process.cwd());
